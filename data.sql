56a354da047c29194e826888d70a194be2731b6e
Michael Marshall <michaelmarshall220@gmail.com>
2022-10-11 17:25:12 -0500

    refactored relationships to use a single table and materialized views for each use case. added data generator.

---
+++ data.sql
@@ -0,0 +1,235 @@
+create or replace procedure load_data()
+language plpgsql
+as $$
+declare
+  v_bible_1_id bigint;
+  v_book_1_index_id bigint;
+  v_book_1_id bigint;
+  v_chapter_1_id bigint;
+  v_verse_1_id bigint;
+  v_word_index_1_id_1 bigint;
+  v_word_index_1_id_2 bigint;
+  v_word_index_1_id_3 bigint;
+  v_word_index_1_id_4 bigint;
+  v_word_ref_1_id_1 bigint;
+  v_word_ref_1_id_2 bigint;
+  v_word_ref_1_id_3 bigint;
+  v_word_ref_1_id_4 bigint;
+
+  v_bible_2_id bigint;
+  v_book_2_index_id bigint;
+  v_book_2_id bigint;
+  v_chapter_2_id bigint;
+  v_verse_2_id bigint;
+  v_word_index_2_id_1 bigint;
+  v_word_index_2_id_2 bigint;
+  v_word_index_2_id_3 bigint;
+  v_word_index_2_id_4 bigint;
+  v_word_ref_2_id_1 bigint;
+  v_word_ref_2_id_2 bigint;
+  v_word_ref_2_id_3 bigint;
+  v_word_ref_2_id_4 bigint;
+begin
+  -- document 1
+  insert into usq_bibles(language_index, language_id, name)
+  values ('_fake_index_1', 1, 'us_ENG')
+  on conflict do nothing
+  returning bible_id
+  into v_bible_1_id;
+
+  if v_bible_1_id is null then
+    raise notice 'v_bible_1_id is null %', now();
+    return;
+  end if;
+
+  insert into usq_book_index(name)
+  values ('Genesis')
+  returning book_index_id
+  into v_book_1_index_id;
+
+  insert into usq_books(bible_id, book_index_id)
+  values (v_bible_1_id, v_book_1_index_id)
+  returning book_id
+  into v_book_1_id;
+
+  insert into usq_chapters(book_id, chapter_number)
+  values (v_book_1_id, 1)
+  returning chapter_id
+  into v_chapter_1_id;
+
+  insert into usq_verses(chapter_id, verse_number)
+  values (v_chapter_1_id, 1)
+  returning verse_id
+  into v_verse_1_id;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_1', 1, 'In')
+  returning word_index_id
+  into v_word_index_1_id_1;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_1', 1, 'the')
+  returning word_index_id
+  into v_word_index_1_id_2;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_1', 1, 'beginning')
+  returning word_index_id
+  into v_word_index_1_id_3;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_1', 1, 'God')
+  returning word_index_id
+  into v_word_index_1_id_4;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_1_id, v_word_index_1_id_1, 1)
+  returning word_ref_id
+  into v_word_ref_1_id_1;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_1_id, v_word_index_1_id_2, 2)
+  returning word_ref_id
+  into v_word_ref_1_id_2;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_1_id, v_word_index_1_id_3, 3)
+  returning word_ref_id
+  into v_word_ref_1_id_3;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_1_id, v_word_index_1_id_4, 4)
+  returning word_ref_id
+  into v_word_ref_1_id_4;
+
+  -- document 2
+  insert into usq_bibles(language_index, language_id, name)
+  values ('_fake_index_2', 1, '_fake twig 1')
+  on conflict do nothing
+  returning bible_id
+  into v_bible_2_id;
+
+  if v_bible_2_id is null then
+    raise notice 'v_bible_2_id is null %', now();
+    return;
+  end if;
+
+  insert into usq_books(bible_id, book_index_id)
+  values (v_bible_2_id, v_book_1_index_id) -- use same book index
+  returning book_id
+  into v_book_2_id;
+
+  insert into usq_chapters(book_id, chapter_number)
+  values (v_book_2_id, 1)
+  returning chapter_id
+  into v_chapter_2_id;
+
+  insert into usq_verses(chapter_id, verse_number)
+  values (v_chapter_2_id, 1)
+  returning verse_id
+  into v_verse_2_id;
+
+  -- different langauge than document 1
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_2', 1, '_fake_In')
+  returning word_index_id
+  into v_word_index_2_id_1;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_2', 1, '_fake_the')
+  returning word_index_id
+  into v_word_index_2_id_2;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_2', 1, '_fake_beginning')
+  returning word_index_id
+  into v_word_index_2_id_3;
+
+  insert into usq_word_index(language_index, language_id, word)
+  values ('_fake_index_2', 1, '_fake_God')
+  returning word_index_id
+  into v_word_index_2_id_4;
+
+  -- note the different word order
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_2_id, v_word_index_2_id_1, 4)
+  returning word_ref_id
+  into v_word_ref_2_id_1;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_2_id, v_word_index_2_id_2, 3)
+  returning word_ref_id
+  into v_word_ref_2_id_2;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_2_id, v_word_index_2_id_3, 2)
+  returning word_ref_id
+  into v_word_ref_2_id_3;
+
+  insert into usq_word_references(verse_id, word_index_id, word_number)
+  values (v_verse_2_id, v_word_index_2_id_4, 1)
+  returning word_ref_id
+  into v_word_ref_2_id_4;
+
+  -- alignment
+  insert into usq_references(
+    ref_type,
+    source_type,
+    source_id,
+    target_type,
+    target_id
+  )
+  values (
+    'ALIGNMENT',
+    'WORD',
+    v_word_index_1_id_1,
+    'WORD',
+    v_word_index_2_id_4
+  );
+
+  insert into usq_references(
+    ref_type,
+    source_type,
+    source_id,
+    target_type,
+    target_id
+  )
+  values (
+    'ALIGNMENT',
+    'WORD',
+    v_word_index_1_id_2,
+    'WORD',
+    v_word_index_2_id_3
+  );
+
+  insert into usq_references(
+    ref_type,
+    source_type,
+    source_id,
+    target_type,
+    target_id
+  )
+  values (
+    'ALIGNMENT',
+    'WORD',
+    v_word_index_1_id_3,
+    'WORD',
+    v_word_index_2_id_2
+  );
+
+  insert into usq_references(
+    ref_type,
+    source_type,
+    source_id,
+    target_type,
+    target_id
+  )
+  values (
+    'ALIGNMENT',
+    'WORD',
+    v_word_index_1_id_4,
+    'WORD',
+    v_word_index_2_id_1
+  );
+
+end; $$;
--- schema.sql
+++ schema.sql
@@ -62,92 +62,134 @@ create table usq_word_references (
   meta jsonb
 );
 
+-- references
+
+create type relationship_type as enum (
+  'RESOURCE',
+  'ALIGNMENT'
+);
+
+create type reference_type as enum (
+  'WORD',
+  'VERSE',
+  'CHAPTER',
+  'BOOK',
+  'BIBLE'
+);
+
+create table usq_references (
+  reference_id bigserial primary key,
+  ref_type relationship_type not null,
+  source_type reference_type not null,
+  source_id bigint not null,
+  target_type reference_type not null,
+  target_id bigint not null,
+  meta jsonb,
+  unique (source_type, source_id, target_type, target_id)
+);
+
+-- we'll create any needed materialized views for hasura to 
+-- use to create graphql apis
+
+-- view name format:
+-- usq_[source]_to_[target]_[relationship_type]
+
+-- alignment
+create materialized view usq_word_to_word_alignment as
+  select reference_id, source_id, target_id, meta
+  from usq_references
+  where 
+    ref_type = 'ALIGNMENT' and
+    source_type = 'WORD' and
+    target_type = 'WORD'
+with no data;
+
 -- Alignment References (two_way)
 
-create type usq_alignment_type as enum (
-  '>', 
-  '<',
-  '=' 
-);
+-- create type usq_alignment_type as enum (
+--   '>', 
+--   '<',
+--   '=' 
+-- );
 
-create table usq_chapter_starts_alignment (
-  chapter_starts_alignment_id bigserial primary key,
-  chapter_id_1 bigint not null references usq_chapters(chapter_id),
-  chapter_id_2 bigint not null references usq_chapters(chapter_id),
-  alignment usq_alignment_type not null,
-  meta jsonb,
-  unique (chapter_id_1, chapter_id_2)
-);
+-- create table usq_chapter_starts_alignment (
+--   chapter_starts_alignment_id bigserial primary key,
+--   chapter_id_1 bigint not null references usq_chapters(chapter_id),
+--   chapter_id_2 bigint not null references usq_chapters(chapter_id),
+--   alignment usq_alignment_type not null,
+--   meta jsonb,
+--   unique (chapter_id_1, chapter_id_2)
+-- );
 
-create table usq_chapter_stops_alignment (
-  chapter_stops_alignment_id bigserial primary key,
-  chapter_id_1 bigint not null references usq_chapters(chapter_id),
-  chapter_id_2 bigint not null references usq_chapters(chapter_id),
-  alignment usq_alignment_type not null,
-  meta jsonb,
-  unique (chapter_id_1, chapter_id_2)
-);
+-- create table usq_chapter_stops_alignment (
+--   chapter_stops_alignment_id bigserial primary key,
+--   chapter_id_1 bigint not null references usq_chapters(chapter_id),
+--   chapter_id_2 bigint not null references usq_chapters(chapter_id),
+--   alignment usq_alignment_type not null,
+--   meta jsonb,
+--   unique (chapter_id_1, chapter_id_2)
+-- );
 
-create table usq_verse_starts_alignment (
-  verse_starts_alignment_id bigserial primary key,
-  verse_id_1 bigint not null references usq_verses(verse_id),
-  verse_id_2 bigint not null references usq_verses(verse_id),
-  alignment usq_alignment_type not null,
-  meta jsonb,
-  unique (verse_id_1, verse_id_2)
-);
+-- create table usq_verse_starts_alignment (
+--   verse_starts_alignment_id bigserial primary key,
+--   verse_id_1 bigint not null references usq_verses(verse_id),
+--   verse_id_2 bigint not null references usq_verses(verse_id),
+--   alignment usq_alignment_type not null,
+--   meta jsonb,
+--   unique (verse_id_1, verse_id_2)
+-- );
 
-create table usq_verse_stops_alignment (
-  verse_stops_alignment_id bigserial primary key,
-  verse_id_1 bigint not null references usq_verses(verse_id),
-  verse_id_2 bigint not null references usq_verses(verse_id),
-  alignment usq_alignment_type not null,
-  meta jsonb,
-  unique (verse_id_1, verse_id_2)
-);
+-- create table usq_verse_stops_alignment (
+--   verse_stops_alignment_id bigserial primary key,
+--   verse_id_1 bigint not null references usq_verses(verse_id),
+--   verse_id_2 bigint not null references usq_verses(verse_id),
+--   alignment usq_alignment_type not null,
+--   meta jsonb,
+--   unique (verse_id_1, verse_id_2)
+-- );
 
 -- Resource References (one-way)
 
-create table usq_word_to_word_references (
-  word_word_ref_id bigserial primary key,
-  source_word bigint not null references usq_word_references(word_ref_id),
-  target_word bigint not null references usq_word_references(word_ref_id),
-  meta jsonb
-);
+-- create table usq_word_to_word_references (
+--   word_word_ref_id bigserial primary key,
+--   source_word bigint not null references usq_word_references(word_ref_id),
+--   target_word bigint not null references usq_word_references(word_ref_id),
+--   meta jsonb
+-- );
 
-create table usq_word_to_verse_references (
-  word_verse_ref_id bigserial primary key,
-  source_word bigint not null references usq_word_references(word_ref_id),
-  target_verse bigint not null references usq_verses(verse_id),
-  meta jsonb
-);
+-- create table usq_word_to_verse_references (
+--   word_verse_ref_id bigserial primary key,
+--   source_word bigint not null references usq_word_references(word_ref_id),
+--   target_verse bigint not null references usq_verses(verse_id),
+--   meta jsonb
+-- );
 
-create table usq_word_to_chapter_references (
-  word_chapter_ref_id bigserial primary key,
-  source_word bigint not null references usq_word_references(word_ref_id),
-  target_chapter bigint not null references usq_chapters(chapter_id),
-  meta jsonb
-);
+-- create table usq_word_to_chapter_references (
+--   word_chapter_ref_id bigserial primary key,
+--   source_word bigint not null references usq_word_references(word_ref_id),
+--   target_chapter bigint not null references usq_chapters(chapter_id),
+--   meta jsonb
+-- );
 
-create table usq_verse_to_verse_references (
-  verse_verse_ref_id bigserial primary key,
-  source_verse bigint not null references usq_verses(verse_id),
-  target_verse bigint not null references usq_verses(verse_id),
-  meta jsonb
-);
+-- create table usq_verse_to_verse_references (
+--   verse_verse_ref_id bigserial primary key,
+--   source_verse bigint not null references usq_verses(verse_id),
+--   target_verse bigint not null references usq_verses(verse_id),
+--   meta jsonb
+-- );
 
-create table usq_verse_to_chapter_references (
-  verse_chapter_ref_id bigserial primary key,
-  source_verse bigint not null references usq_verses(verse_id),
-  target_chapter bigint not null references usq_chapters(chapter_id),
-  meta jsonb
-);
+-- create table usq_verse_to_chapter_references (
+--   verse_chapter_ref_id bigserial primary key,
+--   source_verse bigint not null references usq_verses(verse_id),
+--   target_chapter bigint not null references usq_chapters(chapter_id),
+--   meta jsonb
+-- );
 
-create table usq_book_to_chapter_references (
-  book_chapter_ref_id bigserial primary key,
-  source_book bigint not null references usq_books(book_id),
-  target_chapter bigint not null references usq_chapters(chapter_id),
-  meta jsonb
-);
+-- create table usq_book_to_chapter_references (
+--   book_chapter_ref_id bigserial primary key,
+--   source_book bigint not null references usq_books(book_id),
+--   target_chapter bigint not null references usq_chapters(chapter_id),
+--   meta jsonb
+-- );
 
 -- more types of reference tables can be created as needed

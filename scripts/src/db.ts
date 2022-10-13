c9160329a75f71a0e41ec5aa57e8ddafa4842817
Jake Shilling <shilling.jake@gmail.com>
2022-10-12 20:24:39 -0400

    Create functions to turn the SQL template string into a real query

--- scripts/src/db.ts
+++ scripts/src/db.ts
@@ -1,4 +1,18 @@
 import { IUSFMParsedObject } from 'usfm-grammar';
+
+interface IInsertQueryParamters {
+  language_index: string;
+  language_id: number;
+  collection_name: string;
+  book_name: string;
+}
+
+interface ICollectionParameters {
+  language_index: string;
+  language_id: number;
+  collection_name: string
+}
+
 const INSERT_QUERY_TEMPLATE = `
 WITH collection AS (
      SELECT collection_id
@@ -20,5 +34,31 @@ FROM collection
 CROSS JOIN book_index;
 `;
 
+function getInsertQuery(params: IInsertQueryParamters) {
+  return {
+    text: INSERT_QUERY_TEMPLATE,
+    values: [
+      params.language_index,
+      params.language_id,
+      params.collection_name,
+      params.book_name
+    ]
+  }
 }
 
+function getInsertQueryParams(
+    collection: ICollectionParameters,
+    input: IUSFMParsedObject
+): IInsertQueryParamters {
+
+  return {
+      ...collection,
+      book_name: input.book.bookCode
+  };
+
+}
+
+export {
+    getInsertQuery,
+    getInsertQueryParams
+};

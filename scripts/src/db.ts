67df29407417f28e1e76d2154f40cc070958e04c
Jake Shilling <shilling.jake@gmail.com>
2022-10-12 20:22:58 -0400

    Create template string to insert book names
    This should define the basic structure of what to come. It uses CTEs
    to insert multiple dependant rows across several tables in a single
    query. This simple implementation inserts a new book and linking it to
    an existing collection.

--- scripts/src/db.ts
+++ scripts/src/db.ts
@@ -1,4 +1,24 @@
 import { IUSFMParsedObject } from 'usfm-grammar';
+const INSERT_QUERY_TEMPLATE = `
+WITH collection AS (
+     SELECT collection_id
+     FROM usq_collections
+     WHERE language_index = $1
+       AND language_id    = $2
+       AND name           = $3
+),
+book_index AS (
+     INSERT INTO usq_book_index(name)
+     VALUES ($4)
+     ON CONFLICT (name) DO UPDATE
+        SET name = excluded.name
+     RETURNING book_index_id
+)
+INSERT INTO usq_books(collection_id, book_index_id)
+SELECT collection.collection_id, book_index.book_index_id
+FROM collection
+CROSS JOIN book_index;
+`;
 
 }
 
